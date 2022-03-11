4d7944a175104616e3cdc6489fbc224278bad709
StowasserH <git@stowasser.tv>
2022-03-11 11:11:52 +0100

    set height for textlines

--- pylcars/widgets/textline.py
+++ pylcars/widgets/textline.py
@@ -7,11 +7,14 @@ import os
 
 
 class Textline(Deco):
-    def __init__(self, lcars, rect, text_color, svg=None, style=None):
+    def __init__(self, lcars, rect, text_color, text_height, svg=None, style=None):
         if not style:
             style = self.default_style
-        self.mainstyle=style
-        Deco.__init__(self, lcars, rect, text_color, svg, style + "color: " +  text_color + ";")
+        self.mainstyle = style
+        Deco.__init__(self, lcars, rect, text_color, svg, style + "color: " + text_color + ";")
+        newfont = QtGui.QFont(self.default_font_name, text_height)
+        newfont.setLetterSpacing(QtGui.QFont.AbsoluteSpacing, 1)
+        self.setFont(newfont)
 
-    def change_color(self,color):
-        self.setStyleSheet(self.mainstyle + "color: " +  color + ";")
+    def change_color(self, color):
+        self.setStyleSheet(self.mainstyle + "color: " + color + ";")
